acb20b7c46df83c1b7e11b04d22f01bf2c2a3d40
daeyeob kim <kimdyk2@hanmail.net>
2022-02-27 23:26:11 +0900

    22.02.27 업로드 => 향후 보충 요망

---
+++ styling-with-styled-components/src/App.js
@@ -0,0 +1,108 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+function App() {
+  return (
+    <ThemeProvider // styled-components로 만드는 모든 컴포넌트에서 조회하여 사용할 수 있는 전역적인 값 설정
+      theme={{
+        // theme 설정시 ThemeProvider 내부에 랜더링된 styled-components로 만든 컴포넌트에서 palette를 조회하여 사용할 수 있음
+        palette: {
+          blue: "#228be6",
+          gray: "#495057",
+          pink: "#f06595",
+        },
+      }}
+    >
+      <AppBlock>
+        <Button>BUTTON</Button>
+      </AppBlock>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+/*
+import React from "react";
+import styled from "styled-components";
+import Button from "./components/Button";
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+function App() {
+  return (
+    <AppBlock>
+      <Button>BUTTON</Button>
+    </AppBlock>
+  );
+}
+
+export default App;
+*/
+
+/*
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color || "black"};
+  border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
+
+function App() {
+  return <Circle color="red" huge />;
+}
+
+export default App;
+*/
+
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
